3d22750432a2f585ee5cf1033508f9d31be5e0a5
ToleonPrins <114396706+ToleonPrins@users.noreply.github.com>
2023-08-29 11:08:06 +0700

    Initial Commit

---
+++ MultiLogin/MultiLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiLoginApp.swift
+//  MultiLogin
+//
+//  Created by Алексей Иванников on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MultiLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
